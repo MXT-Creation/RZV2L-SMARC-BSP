MXT-Creation/RZV2L-SMARC-BSP

commit cb3c4e10f9407a937125ab1f25a97ae82233ac73

Add camera drivers

meta-mxt-rzv2l/recipes-kernel/linux/linux-renesas_5.10.bbappend
@@ -1,11 +1,14 @@
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 
 KERNEL_DEVICETREE = " \
+        renesas/r9a07g054l2-ov13850-smarc.dtb \
 	renesas/r9a07g054l2-mxt-smarc-ov5647.dtb \
 	renesas/r9a07g054l2-mxt-smarc-imx219.dtb \
 	renesas/r9a07g054l2-mxt-smarc-imx708.dtb \
 	renesas/r9a07g054l2-mxt-smarc-tevs.dtb \
 	renesas/r9a07g054l2-mxt-smarc.dtb \
+	renesas/r9a07g054l2-pivariety-smarc.dtb \
+	renesas/r9a07g054l2-imx135-smarc.dtb \
 "
 
 SRC_URI_append +=  "\
@@ -13,10 +16,7 @@ SRC_URI_append +=  "\
 	file://fragment-01-usb-ethernet.cfg \
 	file://fragment-02-wifi.cfg \
 	file://fragment-03-can.cfg \
-	file://fragment-04-ov5647.cfg \
-	file://fragment-05-imx219.cfg \
-	file://fragment-06-mscc-phy.cfg \
-	file://fragment-07-tevs.cfg \
+	file://fragment-04-camera-sensors.cfg \
 	file://patches/0002-media-ov5647-Add-support-for-PWDN-GPIO.patch \
 	file://patches/0003-media-ov5647-Add-support-for-non-continuous-clock-mo.patch \
 	file://patches/0004-media-ov5647-Add-set_fmt-and-get_fmt-calls.patch \
@@ -50,6 +50,9 @@ SRC_URI_append +=  "\
 	file://patches/1001-media-i2c-imx477-port-driver-from-RPi.patch \
 	file://patches/1002-rzv2l-add-support-for-imx708.patch \
 	file://patches/2001-drivers-media-i2c-tevs-backport-driver-from-TechNexi.patch \
+	file://patches/2002-media-i2c-backport-Arducam-Pivariety-64MP-camera-s-s.patch \
+	file://patches/2003-media-i2c-add-support-for-Sony-IMX135.patch \
+	file://patches/2004-media-i2c-ov13850-add-driver-for-OmniVision-OV13850.patch \
 "
 
 do_compile_prepend() {
@@ -59,5 +62,5 @@ do_compile_prepend() {
 do_install_append() {
 	# This way we get a booting system, even if the camera is not the same
 	install -m 0755 -d ${D}/boot
-	cp ${D}/boot/r9a07g054l2-mxt-smarc.dtb ${D}/boot/r9a07g054l2-smarc.dtb
+	cp ${D}/boot/r9a07g054l2-ov13850-smarc.dtb ${D}/boot/r9a07g054l2-smarc.dtb
 }

meta-mxt-rzv2l/recipes-app/app-usbcam-http/app-usbcam-http.bb
@@ -12,7 +12,7 @@ SRC_URI = "file://src/ \
 	   file://app-usbcam-http.service \
 "
 
-DEPENDS += " drpai libjpeg-turbo "
+DEPENDS += " drpai libjpeg-turbo opencv "
 
 RDEPENDS_${PN} += " drpai-models "
 

meta-mxt-rzv2l/recipes-app/app-usbcam-http/app-usbcam-http/src/CMakeLists.txt
@@ -32,3 +32,7 @@ TARGET_LINK_LIBRARIES(sample_app_usbcam_http pthread)
 TARGET_LINK_LIBRARIES(sample_app_usbcam_http turbojpeg)
 TARGET_LINK_LIBRARIES(sample_app_usbcam_http jpeg)
 
+find_package(OpenCV REQUIRED)
+include_directories(${OpenCV_INCLUDE_DIRS})
+target_link_libraries(sample_app_usbcam_http ${OpenCV_LIBS})
+

meta-mxt-rzv2l/recipes-app/app-usbcam-http/app-usbcam-http/src/camera/camera.cpp
@@ -34,8 +34,8 @@
 
 Camera::Camera()
 {
-    camera_width = 0;
-    camera_height = 0;
+    out_width = 0;
+    out_height = 0;
     camera_color = 0;
 }
 
@@ -92,6 +92,62 @@ static uint64_t calc_umdabuf_addr()
     return ret_address;
 }
 
+void Camera::read_native_resolutions()
+{
+    in_width = out_width;
+    in_height = out_height;
+
+    std::ifstream file("/tmp/app-usbcam-http-config");
+    if (!file)
+        return;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        // Skip empty lines and lines starting with '#'
+        if (line.empty() || line[0] == '#') {
+            continue;
+        }
+
+        // Find the position of the '=' delimiter
+        size_t delimiterPos = line.find('=');
+        if (delimiterPos != std::string::npos) {
+            // Extract the key and value
+            std::string key = line.substr(0, delimiterPos);
+            std::string value = line.substr(delimiterPos + 1);
+            if (key == "NATIVE_CAMERA_IMAGE_WIDTH")
+                in_width = std::stoi(value);
+            else if (key == "NATIVE_CAMERA_IMAGE_HEIGHT")
+                in_height = std::stoi(value);
+        }
+    }
+
+    // Close the file
+    file.close();
+
+    if (in_width == out_width && out_height == in_height) {
+        adjust_img = false;
+        return;
+    }
+
+    double ws = (double) in_width / (double) out_width;
+    double hs = (double) in_height / (double) out_height;
+    if (ws < hs) {
+        scale_width = out_width;
+        scale_height = in_height / ws;
+        int crop_x = 0;
+        int crop_y = ((scale_height - out_height) & ~1) / 2;
+        crop_region = cv::Rect(crop_x, crop_y, 640, 480);
+    } else {
+        scale_width = in_width / hs;
+        scale_height = out_height;
+        int crop_x = ((scale_width - out_width) & ~1) / 2;
+        int crop_y = 0;
+        crop_region = cv::Rect(crop_x, crop_y, 640, 480);
+    }
+
+    adjust_img = true;
+}
+
 /**
  * @brief start_camera
  * @details  Function to initialize USB camera capture
@@ -104,8 +160,13 @@ int8_t Camera::start_camera()
     int32_t i = 0;
     int32_t n = 0;
 
-    printf("Camera width = %d\n", camera_width);
-    printf("Camera height = %d\n", camera_height);
+    read_native_resolutions();
+
+    printf("Camera (input) width = %d\n", in_width);
+    printf("Camera (input) height = %d\n", in_height);
+
+    printf("Camera (output) width = %d\n", out_width);
+    printf("Camera (output) height = %d\n", out_height);
     printf("Camera channel = %d\n", camera_color);
 
     ret = open_camera_device();
@@ -381,8 +442,8 @@ int8_t Camera::init_camera_fmt()
     struct v4l2_format fmt;
     memset(&fmt, 0, sizeof(fmt));
     fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    fmt.fmt.pix.width = camera_width;
-    fmt.fmt.pix.height = camera_height;
+    fmt.fmt.pix.width = in_width;
+    fmt.fmt.pix.height = in_height;
     fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
     fmt.fmt.pix.field = V4L2_FIELD_NONE;
 
@@ -538,6 +599,39 @@ int8_t Camera::inference_capture_qbuf()
     return 0;
 }
 
+void rgb_to_yuv422_uyvy(const cv::Mat& rgb, cv::Mat& yuv) {
+    assert(rgb.size() == yuv.size() &&
+           rgb.depth() == CV_8U &&
+           rgb.channels() == 3 &&
+           yuv.depth() == CV_8U &&
+           yuv.channels() == 2);
+    for (int ih = 0; ih < rgb.rows; ih++) {
+        const uint8_t* rgbRowPtr = rgb.ptr<uint8_t>(ih);
+        uint8_t* yuvRowPtr = yuv.ptr<uint8_t>(ih);
+
+        for (int iw = 0; iw < rgb.cols; iw = iw + 2) {
+            const int rgbColIdxBytes = iw * rgb.elemSize();
+            const int yuvColIdxBytes = iw * yuv.elemSize();
+
+            const uint8_t R1 = rgbRowPtr[rgbColIdxBytes + 0];
+            const uint8_t G1 = rgbRowPtr[rgbColIdxBytes + 1];
+            const uint8_t B1 = rgbRowPtr[rgbColIdxBytes + 2];
+            const uint8_t R2 = rgbRowPtr[rgbColIdxBytes + 3];
+            const uint8_t G2 = rgbRowPtr[rgbColIdxBytes + 4];
+            const uint8_t B2 = rgbRowPtr[rgbColIdxBytes + 5];
+
+            const int Y  =  (0.257f * R1) + (0.504f * G1) + (0.098f * B1) + 16.0f ;
+            const int U  = -(0.148f * R1) - (0.291f * G1) + (0.439f * B1) + 128.0f;
+            const int V  =  (0.439f * R1) - (0.368f * G1) - (0.071f * B1) + 128.0f;
+            const int Y2 =  (0.257f * R2) + (0.504f * G2) + (0.098f * B2) + 16.0f ;
+
+            yuvRowPtr[yuvColIdxBytes + 0] = cv::saturate_cast<uint8_t>(Y );
+            yuvRowPtr[yuvColIdxBytes + 1] = cv::saturate_cast<uint8_t>(U );
+            yuvRowPtr[yuvColIdxBytes + 2] = cv::saturate_cast<uint8_t>(Y2);
+            yuvRowPtr[yuvColIdxBytes + 3] = cv::saturate_cast<uint8_t>(V );
+        }
+    }
+}
 
 /**
  * @brief get_img
@@ -546,6 +640,24 @@ int8_t Camera::inference_capture_qbuf()
  */
 uint8_t* Camera::get_img()
 {
+    static int last_index = -1;
+    if (!adjust_img || last_index == buf_capture.index)
+        return buffer[buf_capture.index];
+
+    cv::Mat orig(in_height, in_width, CV_8UC2, buffer[buf_capture.index]);
+    cv::Mat origRgb;
+    cv::cvtColor(orig, origRgb, cv::COLOR_YUV2RGB_YUYV);
+
+    cv::Mat scaled;
+    cv::resize(origRgb, scaled, cv::Size(scale_width, scale_height));
+    auto cropped = scaled(crop_region);
+
+    cv::Mat final_out(out_height, out_width, CV_8UC2, buffer[buf_capture.index]);
+
+    rgb_to_yuv422_uyvy(cropped, final_out);
+
+    last_index = buf_capture.index;
+
     return buffer[buf_capture.index];
 }
 
@@ -562,59 +674,26 @@ int32_t Camera::get_size()
 
 
 /**
- * @brief get_w
- * @details Get camera_width. This function is currently NOT USED.
- * @return int32_t width of camera capture image.
- */
-int32_t Camera::get_w()
-{
-    return camera_width;
-}
-
-
-/**
  * @brief set_w
- * @details Set camera_width. This function is currently NOT USED.
+ * @details Set out_width. This function is currently NOT USED.
  * @param w new camera capture image width
  */
 void Camera::set_w(int32_t w)
 {
-    camera_width = w;
+    out_width = w;
     return;
-}
-
-
-/**
- * @brief get_h
- * @details Get camera_height. This function is currently NOT USED.
- * @return int32_t height of camera capture image.
- */
-int32_t Camera::get_h()
-{
-    return camera_height;
 }
 
 
 /**
  * @brief set_h
- * @details Set camera_height. This function is currently NOT USED.
+ * @details Set out_height. This function is currently NOT USED.
  * @param h new camera capture image height
  */
 void Camera::set_h(int32_t h)
 {
-    camera_height = h;
+    out_height = h;
     return;
-}
-
-
-/**
- * @brief get_c
- * @details Get camera_color. This function is currently NOT USED.
- * @return int32_t color channel of camera capture image.
- */
-int32_t Camera::get_c()
-{
-    return camera_color;
 }
 
 

meta-mxt-rzv2l/recipes-app/app-usbcam-http/app-usbcam-http/src/camera/camera.h
@@ -25,6 +25,7 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <opencv2/opencv.hpp>
 #include <linux/videodev2.h>
 #include "define.h"
 
@@ -47,18 +48,23 @@ class Camera
 
         uint8_t * get_img();
         int32_t get_size();
-        int32_t get_w();
         void set_w(int32_t w);
-        int32_t get_h();
         void set_h(int32_t h);
-        int32_t get_c();
         void set_c(int32_t c);
         bool _using_inf;
 
     private:
+        bool adjust_img;
+        int32_t scale_width;
+        int32_t scale_height;
+        cv::Rect crop_region;
+
+        void read_native_resolutions();
         std::string device;
-        int32_t camera_width;
-        int32_t camera_height;
+        int32_t in_width;
+        int32_t in_height;
+        int32_t out_width;
+        int32_t out_height;
         int32_t camera_color;
         int8_t m_fd;
         int32_t imageLength;

meta-mxt-rzv2l/recipes-app/app-usbcam-http/app-usbcam-http/src/recognize/recognize_base.cpp
@@ -288,8 +288,7 @@ void* RecognizeBase::capture_thread(void* arg)
         if (0 == capture_addr)
         {
             fprintf(stderr, "[ERROR] Failed to _capture image from camera.\n");
-            me->send_app_message("Failed to _capture image from camera.\nRestart the application.");
-            break;
+            continue;
         }
         else
         {
@@ -336,8 +335,7 @@ void* RecognizeBase::capture_thread(void* arg)
         if (0 != ret)
         {
             fprintf(stderr, "[ERROR] Failed to enqueue _capture buffer.\n");
-            me->send_app_message("Failed to enqueue _capture buffer.\nRestart the application.");
-            break;
+            continue;
         }
     } /*End of Loop*/
 

meta-mxt-rzv2l/recipes-custom/custom-configs/files/mxt-rzv2l-camera-init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -f /tmp/app-usbcam-http-config
+
 if media-ctl -d /dev/media0 -V "'tevs 0-0048':0 [fmt:UYVY8_2X8/640x480 field:none]" &> /dev/null ; then
 	echo "Using camera is 'tevs 0-0048':0"
 	media-ctl -d /dev/media0 -V "'rzg2l_csi2 10830400.csi2':1 [fmt:UYVY8_2X8/640x480 field:none]"
@@ -57,6 +59,27 @@ if media-ctl -d /dev/media0 -V "'imx708_noir':0 [fmt:SRGGB10_1X10/1536x864 field
 
 	v4l2-ctl --set-ctrl=digital_gain=2000
 	v4l2-ctl --set-ctrl=analogue_gain=700
+
+	exit 0
+fi
+
+if media-ctl -d /dev/media0 -V "'arducam-pivariety 1-000c':0 [fmt:SRGGB10_1X10/1920x1080 field:none]" &> /dev/null ; then
+	echo "Using camera is 'arducam-pivariety 1-000c':0"
+	media-ctl -d /dev/media0 -V "'rzg2l_csi2 10830400.csi2':1 [fmt:SRGGB10_1X10/1920x1080 field:none]"
+	media-ctl -d /dev/media0 -l "'rzg2l_csi2 10830400.csi2':1 -> 'CRU output':0 [1]"
+
+	exit 0
+fi
+
+if media-ctl -d /dev/media0 -V "'ov13850 0-0036':0 [fmt:SBGGR10_1X10/2112x1568 field:none crop:(96,244)/1920x1080]" &> /dev/null ; then
+	echo "Using camera is 'ov13850 0-0036':0"
+	media-ctl -d /dev/media0 -V "'rzg2l_csi2 10830400.csi2':1 [fmt:SBGGR10_1X10/1920x1080 field:none]"
+	media-ctl -d /dev/media0 -l "'rzg2l_csi2 10830400.csi2':1 -> 'CRU output':0 [1]"
+
+	v4l2-ctl --set-ctrl=analogue_gain=500
+
+	echo "NATIVE_CAMERA_IMAGE_WIDTH=1920" >> /tmp/app-usbcam-http-config
+	echo "NATIVE_CAMERA_IMAGE_HEIGHT=1080" >> /tmp/app-usbcam-http-config
 
 	exit 0
 fi

meta-mxt-rzv2l/recipes-kernel/linux/linux-renesas/dts/r9a07g054l2-imx135-smarc.dts
@@ -1,0 +1,86 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/*
+ * Device Tree Source for the RZ/V2L SMARC MXT board variant
+ *
+ * Copyright (C) 2024 MXT Creation SRL
+ */
+
+/dts-v1/;
+#include "r9a07g054l2-mxt-smarc.dts"
+
+&clk_ext_camera {
+	compatible = "fixed-clock";
+	#clock-cells = <0>;
+	clock-frequency = <24000000>;
+};
+
+/ {
+	cam1_1v8_supply: 1p8v {
+		compatible = "regulator-fixed";
+		regulator-name = "cam1_1v8_supply";
+		regulator-min-microvolt = <1800000>;
+		regulator-max-microvolt = <1800000>;
+		regulator-always-on;
+	};
+
+	cam1_2v8_supply: 2p8v {
+		compatible = "regulator-fixed";
+		regulator-name = "cam1_2v8_supply";
+		regulator-min-microvolt = <2800000>;
+		regulator-max-microvolt = <2800000>;
+		gpios = <&pinctrl RZG2L_GPIO(10, 0) 0>;
+		startup-delay-us = <300000>;
+	};
+
+	cam1_1v2_supply: 1p2v {
+		compatible = "regulator-fixed";
+		regulator-name = "cam1_1v2_supply";
+		regulator-min-microvolt = <1200000>;
+		regulator-max-microvolt = <1200000>;
+		regulator-always-on;
+	};
+};
+
+&i2c0 {
+	pinctrl-0 = <&i2c0_pins>;
+	pinctrl-names = "default";
+
+	status = "okay";
+
+	imx135: imx135@10 {
+		compatible = "sony,imx135";
+		reg = <0x10>;
+		clock-names = "xclk";
+		clocks = <&clk_ext_camera>;
+		clock-frequency = <24000000>;
+		VANA-supply = <&cam1_2v8_supply>;
+		VDIG-supply = <&cam1_1v8_supply>;
+		VDDL-supply = <&cam1_1v2_supply>;
+
+		port {
+			cam1_to_csi: endpoint {
+				clock-lanes = <0>;
+				data-lanes = <1 2>;
+				remote-endpoint = <&csi2_in>;
+				clock-noncontinuous;
+				link-frequencies = /bits/ 64 <456000000>;
+			};
+		};
+	};
+	
+	/delete-node/ camera@3c;
+};
+
+&csi2 {
+	status = "okay";
+
+	ports {
+		port {
+			csi2_in: endpoint {
+				clock-lanes = <0>;
+				data-lanes = <1 2>;
+				remote-endpoint = <&cam1_to_csi>;
+			};
+		};
+	};
+};

commit 2c603cb63724d11445a1de874c13b6dd21d21a26

meta-mxt-rzv2l: kernel: re-enable Microsemi PHY driver
Fixes: cb3c4e1 ("Add camera drivers")
Signed-off-by: Alexandru Ardelean <alex@shruggie.ro>

meta-mxt-rzv2l/recipes-kernel/linux/linux-renesas_5.10.bbappend
@@ -17,6 +17,7 @@ SRC_URI_append +=  "\
 	file://fragment-02-wifi.cfg \
 	file://fragment-03-can.cfg \
 	file://fragment-04-camera-sensors.cfg \
+	file://fragment-06-mscc-phy.cfg \
 	file://patches/0002-media-ov5647-Add-support-for-PWDN-GPIO.patch \
 	file://patches/0003-media-ov5647-Add-support-for-non-continuous-clock-mo.patch \
 	file://patches/0004-media-ov5647-Add-set_fmt-and-get_fmt-calls.patch \